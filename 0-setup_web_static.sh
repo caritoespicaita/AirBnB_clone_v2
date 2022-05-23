c255a47642334b13ae0329b470103b977c004411
caritoespicaita <caritoespicaita@gmail.com>
2022-05-23 14:00:43 -0500

    fix task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,12 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 
 # Create the next folders
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
 
 #Create a fake HTML file
 echo "<html>
@@ -17,13 +21,13 @@ echo "<html>
 </html>">> /data/web_static/releases/test/index.html
 
 # Create a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data
 
 # Update the Nginx configuration to serve the content
-sudo sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
 # Restart nginx
 sudo service nginx restart
