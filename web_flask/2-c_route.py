cb0cc27a0f8cfd43f8c2c301d8dc99ef6bb9fdb5
caritoespicaita <caritoespicaita@hotmail.com>
2022-05-25 00:35:37 -0500

    update task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Starts Flask. """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """ Hello flask. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """ HBNB flask. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ c flask. """
+    return 'c {}'.format(text.replace('_',' '))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
