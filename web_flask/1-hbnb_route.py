41acb8975abdd560c57304011fb7693a06bdaaf8
caritoespicaita <caritoespicaita@hotmail.com>
2022-05-25 00:08:26 -0500

    update task 0 and 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,15 +3,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_route():
     """ Hello flask. """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
     """ HBNB flask. """
     return 'HBNB'
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
