33cfbbedef0a067e30fb67d87b41f5ef8cd14e41
caritoespicaita <caritoespicaita@hotmail.com>
2022-05-25 15:50:32 -0500

    upload task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Starts Flask. """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """ Hello flask. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """ HBNB flask. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ c flask. """
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def phyton_route(text="is cool"):
+    """ phyton flask. """
+    return 'Phyton {}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run()
